25659a816ec50621d87ec2ab66b14110686d8208
Rowland Rose <rowlandrose@gmail.com>
2015-09-10 21:28:51 -0400

    Testing timing
    process.hrtime testing

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello world from Rowland!');
+console.log('Testing process timing in node');
+var time = process.hrtime();
+// [ 1800216, 25 ]
+
+setTimeout(function() {
+  var diff = process.hrtime(time);
+  // [ 1, 552 ]
+
+  console.log('benchmark took %d nanoseconds', diff[0] * 1e9 + diff[1]);
+  // benchmark took 1000000527 nanoseconds
+}, 1000);
