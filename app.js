a87f118339c840e04611c7954e8962afcb4a6260
Rowland Rose <rowlandrose@gmail.com>
2015-09-11 17:33:44 -0400

    Testing midi
    Working test of midi input via UX16

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-console.log('Testing process timing in node');
+// Constants
+var UX16_PORT = 1; // midi port UX16 happens to be on
+
+/*console.log('Testing process timing in node');
 var time = process.hrtime();
 // [ 1800216, 25 ]
 
@@ -8,4 +11,40 @@ setTimeout(function() {
 
   console.log('benchmark took %d nanoseconds', diff[0] * 1e9 + diff[1]);
   // benchmark took 1000000527 nanoseconds
-}, 1000);
+}, 1000);*/
+var midi = require('midi');
+
+// Set up a new input.
+var input = new midi.input();
+
+// Count the available input ports.
+input.getPortCount();
+
+// Get the name of a specified input port.
+//console.log(input.getPortName(1));
+
+// Configure a callback.
+input.on('message', function(deltaTime, message) {
+  // The message is an array of numbers corresponding to the MIDI bytes:
+  //   [status, data1, data2]
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+  // information interpreting the messages.
+  console.log('m:' + message + ' d:' + deltaTime);
+});
+
+// Open the first available input port.
+input.openPort(UX16_PORT);
+
+// Sysex, timing, and active sensing messages are ignored
+// by default. To enable these message types, pass false for
+// the appropriate type in the function below.
+// Order: (Sysex, Timing, Active Sensing)
+// For example if you want to receive only MIDI Clock beats
+// you should use
+// input.ignoreTypes(true, false, true)
+input.ignoreTypes(false, false, false);
+
+// ... receive MIDI messages ...
+
+// Close the port when done.
+//input.closePort();
