51f837c21c1642a2e5eea00cefa89c19aaf5956d
Rowland Rose <rowlandrose@gmail.com>
2015-09-17 21:29:49 -0400

    Track 3 complete
    Track 3 has ‘r’ values producing proper random notes. MIDI control
    surface also able to control it now.

--- app.js
+++ app.js
@@ -213,10 +213,20 @@ var current_note_t4 = 0;
 var current_note_t5 = 0;
 var current_note_t6 = 0;
 
-var current_t3_num = 0;
-var current_t4_num = 0;
-var current_t5_num = 0;
-var current_t6_num = 0;
+var que_note_t3 = 0;
+var que_note_t4 = 0;
+var que_note_t5 = 0;
+var que_note_t6 = 0;
+
+var current_t3_num = current_pattern_t3;
+var current_t4_num = current_pattern_t4;
+var current_t5_num = current_pattern_t5;
+var current_t6_num = current_pattern_t6;
+
+var prev_t3_p = 0;
+var prev_t4_p = 0;
+var prev_t5_p = 0;
+var prev_t6_p = 0;
 
 // Constants
 var MIDI_IO_PORT = 1; // midi port UX16 happens to be on
@@ -234,7 +244,7 @@ var midi = require('midi'); // Include midi library
 
 var current_clock = CLOCK_PER_CLICK;
 var current_pulse = 0;
-var root_note = 60;
+var root_note = 48;
 var current_chord_progression = 1; // 0 to 15
 var prog_spot = 0;
 var current_chord = chord_progressions[current_chord_progression][prog_spot];
@@ -255,27 +265,73 @@ midi_input.on('message', function(deltaTime, message) {
   //console.log(message);
 
   // Get Pattern for Track 1
-  if(message[0] == 180 && message[1] == 24) {
+  if(message[0] == 176 && message[1] == 24) {
     selected_pattern_t1 = message[2];
   }
-  if(message[0] == 180 && message[1] == 113 && message[2] > 0) {
+  if(message[0] == 176 && message[1] == 113 && message[2] > 0) {
     que_pattern_t1 = selected_pattern_t1;
   }
   // Get Random for Track 1
-  if(message[0] == 180 && message[1] == 3) {
+  if(message[0] == 176 && message[1] == 3) {
     current_rand_t1 = message[2];
   }
 
   // Get Pattern for Track 2
-  if(message[0] == 180 && message[1] == 25) {
+  if(message[0] == 176 && message[1] == 25) {
     selected_pattern_t2 = message[2];
   }
-  if(message[0] == 180 && message[1] == 114 && message[2] > 0) {
+  if(message[0] == 176 && message[1] == 114 && message[2] > 0) {
     que_pattern_t2 = selected_pattern_t2;
   }
   // Get Random for Track 2
-  if(message[0] == 180 && message[1] == 9) {
+  if(message[0] == 176 && message[1] == 9) {
     current_rand_t2 = message[2];
+  }
+
+  // Get Pattern for Track 3
+  if(message[0] == 176 && message[1] == 26) {
+    selected_pattern_t3 = message[2];
+  }
+  if(message[0] == 176 && message[1] == 115 && message[2] > 0) {
+    que_pattern_t3 = selected_pattern_t3;
+  }
+  // Get Pattern for Track 4
+  if(message[0] == 176 && message[1] == 27) {
+    selected_pattern_t4 = message[2];
+  }
+  if(message[0] == 176 && message[1] == 116 && message[2] > 0) {
+    que_pattern_t4 = selected_pattern_t4;
+  }
+  // Get Pattern for Track 5
+  if(message[0] == 176 && message[1] == 28) {
+    selected_pattern_t5 = message[2];
+  }
+  if(message[0] == 176 && message[1] == 117 && message[2] > 0) {
+    que_pattern_t5 = selected_pattern_t5;
+  }
+  // Get Pattern for Track 6
+  if(message[0] == 176 && message[1] == 29) {
+    selected_pattern_t6 = message[2];
+  }
+  if(message[0] == 176 && message[1] == 118 && message[2] > 0) {
+    que_pattern_t6 = selected_pattern_t6;
+  }
+
+  // Get Note Pattern for Track 3
+  if(message[0] == 176 && message[1] == 14) {
+    que_note_t3 = message[2];
+  }
+  // Get Note Pattern for Track 4
+  if(message[0] == 176 && message[1] == 15) {
+    que_note_t4 = message[2];
+  }
+  // Get Note Pattern for Track 5
+  if(message[0] == 176 && message[1] == 20) {
+    que_note_t5 = message[2];
+  }
+  // Get Note Pattern for Track 6
+  if(message[0] == 176 && message[1] == 21) {
+    que_note_t6 = message[2];
   }
 
   // Sync to external BPM
@@ -355,15 +411,16 @@ function midi_logic_per_tick() {
   //////////
   // Track 2
   var t2_char = p_t2[current_pattern_t2].charAt(r_applied_t2);
-  var t2_notenum = p_t2_letter_to_notenum[t2_char];
-  if(t2_notenum > 0) {
+  var t2_num = p_t2_letter_to_notenum[t2_char];
+  if(t2_num > 0) {
 
     // Timeout on initial message so BD and SN can trigger, NES quirk
-    setTimeout(function(){ midi_output.sendMessage([148, t2_notenum, 127]); }, 10);
-    setTimeout(function(){ midi_output.sendMessage([148, t2_notenum, 0]); }, 250);
+    setTimeout(function(){ midi_output.sendMessage([148, t2_num, 127]); }, 10);
+    setTimeout(function(){ midi_output.sendMessage([148, t2_num, 0]); }, 250);
   }
   //////////
   // Track 3
+  console.log(current_pattern_t3+' '+selected_pattern_t3+' '+que_pattern_t3);
   var t3_char = p_t3[current_pattern_t3].charAt(current_pulse);
 
   var prev_pulse = current_pulse - 1;
@@ -374,22 +431,24 @@ function midi_logic_per_tick() {
 
   if(t3_char == 'x') {
 
-    var chord_num = parseInt(p_t3_n[current_note_t3].charAt(current_pulse));
-    var chord_spot = chord_num % chord_notes[current_chord].length;
+    var chord_n = p_t3_n[current_note_t3].charAt(current_pulse);
+
+    if(chord_n == 'r') {
+      chord_n = Math.floor(Math.random() * 9) + 1;
+    } else {
+      chord_n = parseInt(chord_n);
+    }
+
+    var chord_spot = chord_n % chord_notes[current_chord].length;
     var translated_num = chord_notes[current_chord][chord_spot];
-    console.log('chord_num: '+chord_num);
-    console.log('chord_spot: '+chord_spot);
-    console.log('translated_num: '+translated_num);
-    var chord_octaves = Math.floor(chord_num / chord_notes[current_chord].length);
+    var chord_octaves = Math.floor(chord_n / chord_notes[current_chord].length);
     translated_num += chord_octaves * 12;
-    console.log('chord_octaves: '+chord_octaves);
-    console.log('translated_num: '+translated_num);
-    console.log('-----');
     current_t3_num = translated_num + current_root;
     //current_t3_num = current_root;
 
     // If prev char was 'x', then send midi note off
-    if(t3_char_prev == 'x' || t3_char_prev == '>') {
+    if(t3_char_prev == 'x' || t3_char_prev == '>'
+      || current_pattern_t3 != prev_t3_p) {
       midi_output.sendMessage([128, current_t3_num, 60]);
     }
 
@@ -398,18 +457,31 @@ function midi_logic_per_tick() {
   } else if(t3_char == '-') {
 
     // If prev char was 'x', then send midi note off
-    if(t3_char_prev == 'x' || t3_char_prev == '>') {
+    if(t3_char_prev == 'x' || t3_char_prev == '>'
+      || current_pattern_t3 != prev_t3_p) {
       midi_output.sendMessage([128, current_t3_num, 60]);
     }
-
   }
+
+  prev_t3_p = current_pattern_t3;
 
   //////////////
   // Pulse Logic
   if(current_pulse + 1 == BEATS_PER_MEASURE * PPQ) {
+
     current_pulse = 0;
+
     current_pattern_t1 = que_pattern_t1;
     current_pattern_t2 = que_pattern_t2;
+    current_pattern_t3 = que_pattern_t3;
+    current_pattern_t4 = que_pattern_t4;
+    current_pattern_t5 = que_pattern_t5;
+    current_pattern_t6 = que_pattern_t6;
+
+    current_note_t3 = que_note_t3;
+    current_note_t4 = que_note_t4;
+    current_note_t5 = que_note_t5;
+    current_note_t6 = que_note_t6;
 
     prog_spot++;
     if(prog_spot >= chord_progressions[current_chord_progression].length) {
