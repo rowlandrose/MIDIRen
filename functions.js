2277ae59b4c7f956287ec3787db5f66f3d7ef8c0
Rowland Rose <rowlandrose@gmail.com>
2015-09-18 19:04:37 -0400

    Track 4
    Track 4 Shruthi is now set up

--- functions.js
+++ functions.js
@@ -33,23 +33,23 @@ function midi_logic_per_tick() {
   } else {
     r_applied_t2 = current_pulse;
   }
-  /////////////
-  // Track 1 BD
+  ///////////////////
+  // Track 1 - NES BD
   //console.log('r_applied_t1: '+r_applied_t1+'r_applied_t2, '+r_applied_t2);
   if(p_t1_bd[current_pattern_t1].charAt(r_applied_t1) == 'x') {
 
     midi_output.sendMessage([148, 57, 127]);
     setTimeout(function(){ midi_output.sendMessage([148, 57, 0]); }, 250);
   }
-  /////////////
-  // Track 1 SN
+  ///////////////////
+  // Track 1 - NES SN
   if(p_t1_sn[current_pattern_t1].charAt(r_applied_t1) == 'x') {
 
     midi_output.sendMessage([148, 60, 127]);
     setTimeout(function(){ midi_output.sendMessage([148, 60, 0]); }, 250);
   }
-  //////////
-  // Track 2
+  /////////////////////////////////
+  // Track 2 - Other NES Percussion
   var t2_char = p_t2[current_pattern_t2].charAt(r_applied_t2);
   var t2_num = p_t2_letter_to_notenum[t2_char];
   if(t2_num > 0) {
@@ -58,8 +58,8 @@ function midi_logic_per_tick() {
     setTimeout(function(){ midi_output.sendMessage([148, t2_num, 127]); }, 10);
     setTimeout(function(){ midi_output.sendMessage([148, t2_num, 0]); }, 250);
   }
-  //////////
-  // Track 3
+  //////////////////////////
+  // Track 3 - NES Mega Inst
   console.log(current_pattern_t3+' '+selected_pattern_t3+' '+que_pattern_t3);
   var t3_char = p_t3[current_pattern_t3].charAt(current_pulse);
 
@@ -84,7 +84,6 @@ function midi_logic_per_tick() {
     var chord_octaves = Math.floor(chord_n / chord_notes[current_chord].length);
     translated_num += chord_octaves * 12;
     current_t3_num = translated_num + current_root;
-    //current_t3_num = current_root;
 
     // If prev char was 'x', then send midi note off
     if(t3_char_prev == 'x' || t3_char_prev == '>'
@@ -108,8 +107,52 @@ function midi_logic_per_tick() {
       midi_output.sendMessage([130, current_t3_num-12, 30]);
     }
   }
-
   prev_t3_p = current_pattern_t3;
+
+  //////////////////////
+  // Track 4 - Shruthi-1
+  console.log(current_pattern_t4+' '+selected_pattern_t4+' '+que_pattern_t4);
+  var t4_char = p_t4[current_pattern_t4].charAt(current_pulse);
+
+  var prev_pulse = current_pulse - 1;
+  if(prev_pulse < 0) {
+    prev_pulse = (BEATS_PER_MEASURE * PPQ) - 1;
+  }
+  var t4_char_prev = p_t4[current_pattern_t4].charAt(prev_pulse);
+
+  if(t4_char == 'x') {
+
+    var chord_n = p_t4_n[current_note_t4].charAt(current_pulse);
+
+    if(chord_n == 'r') {
+      chord_n = Math.floor(Math.random() * 9) + 1;
+    } else {
+      chord_n = parseInt(chord_n);
+    }
+
+    var chord_spot = chord_n % chord_notes[current_chord].length;
+    var translated_num = chord_notes[current_chord][chord_spot];
+    var chord_octaves = Math.floor(chord_n / chord_notes[current_chord].length);
+    translated_num += chord_octaves * 12;
+    current_t4_num = translated_num + current_root;
+
+    // If prev char was 'x', then send midi note off
+    if(t4_char_prev == 'x' || t4_char_prev == '>'
+      || current_pattern_t4 != prev_t4_p) {
+      midi_output.sendMessage([141, current_t4_num, 30]);
+    }
+
+    midi_output.sendMessage([157, current_t4_num, 30]);
+
+  } else if(t4_char == '-') {
+
+    // If prev char was 'x', then send midi note off
+    if(t4_char_prev == 'x' || t4_char_prev == '>'
+      || current_pattern_t4 != prev_t4_p) {
+      midi_output.sendMessage([141, current_t4_num, 30]);
+    }
+  }
+  prev_t4_p = current_pattern_t4;
 
   //////////////
   // Pulse Logic
